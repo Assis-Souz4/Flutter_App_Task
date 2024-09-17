26a586e28ce72ff2cb34b9b149dfd0a30674e90b
Assis Souza <assissouza-dev@hotmail.com>
2024-09-16 21:24:41 -0300

    create metodo map for list, findAll

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,6 +2,7 @@ import 'package:flutter_app_task_2024/data/task_dao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
+//achar o caminho e abri e criar o banco caso nâo exista
 Future<Database> getDatabase() async {
   final String path = join(await getDatabasesPath(), 'task.db');
   return openDatabase(path, onCreate: (db, version) {
--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,5 +1,7 @@
 //classe que tem a responsabilidade de comunicação com o banco de dados
 import 'package:flutter_app_task_2024/components/my_task.dart';
+import 'package:flutter_app_task_2024/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   //criação da tabela do db
@@ -18,7 +20,26 @@ class TaskDao {
   //salvar dado
   save(MyTask task) async {}
   //procurar todos os dados
-  Future<List<MyTask>> findAll() async {}
+  Future<List<MyTask>> findAll() async {
+    print('ACCESS FINDALL');
+    final Database bancoDeDados = await getDatabase();
+    //add o resultado da query na variavel result
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tablename);
+    print('QUERY $result');
+    return toList(result);
+  }
+
+  List<MyTask> toList(List<Map<String, dynamic>> mapOfTask) {
+    final List<MyTask> tarefas = [];
+    for (Map<String, dynamic> row in mapOfTask) {
+      final MyTask tarefa = MyTask(row[_name], row[_difficulty], row[_image]);
+      tarefas.add(tarefa);
+    }
+    print('TASK LIST: ${tarefas}');
+    return tarefas;
+  }
+
   //procurar um dado
   Future<List<MyTask>> find(String nameTask) async {}
   delete(String nameTask) async {}
